29ee3ecb2e1e79ea6e43f68536ddf10ca1d30a00
Tim <lai@clustersolutions.net>
2020-04-01 20:44:24 -0700

    Initial commit.

---
+++ JarTest/lait/JarTest.java
@@ -0,0 +1,28 @@
+package lait;
+
+// #Dummy import for dependency test
+// import junit.framework.TestCase;
+
+public class JarTest {
+
+	private Boolean value;
+
+	public void JarTest() {
+
+        this.setValue();
+
+    }
+
+    public void setValue() {
+
+    	this.value = true;
+
+    }
+
+    public Boolean getValue() {
+
+    	return this.value;
+    	
+    }
+
+}  
---
+++ Test.java
@@ -0,0 +1,28 @@
+import lait.JarTest;
+
+class Test {
+ 
+	public static void main(String[] args) 
+	{
+	  	boolean var;
+
+		JarTest obj = new lait.JarTest();
+
+		// var = obj.getValue();
+
+		System.out.println(obj.getValue());
+
+		// if (obj.value == True) 
+		// {
+
+		// 	System.out.println("True");
+
+		// } else 
+		// {
+
+		// 	System.out.println("False");
+		// }
+	 
+	}
+
+}
